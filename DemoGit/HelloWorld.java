c3a0b9fbd51937f631037ca6d153e13f07aa07b8
AlifFadhlurra <alif.fadhlurra@gmail.com>
2021-10-01 16:21:49 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Alif Adzkar Fadhlurrahman");
+		System.out.println("NIM: 6706213158");
+		System.out.println("Class : D3IF4501");
+		}
+	}
